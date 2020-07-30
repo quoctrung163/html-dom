ec9309f1a7e318c9de4deabe333d0696c4069879
quoctrung163 <miyucoder@gmail.com>
2020-07-31 03:17:12 +0700

    1-add or remove class

---
+++ 1-add-or-remove-class/index.js
@@ -0,0 +1,13 @@
+const element1 = document.getElementById('element-1');
+const element2 = document.getElementById('element-2');
+const element3 = document.getElementById('element-3');
+
+// add class
+element1.classList.add('class-1');
+
+// add multiclass
+element2.classList.add('class-2', 'class-order');
+
+// toggle class
+element3.classList.toggle('class-order');
+
