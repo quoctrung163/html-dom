307908e02ea5976a404ec17c1707f3af8796983e
quoctrung163 <miyucoder@gmail.com>
2020-08-02 13:11:12 +0700

    7-change-web-favicon

--- 7-change-web-favicon/index.js
+++ 7-change-web-favicon/index.js
@@ -1,3 +1,4 @@
+// change the website
 const setFavicon = url => {
   // find the current favicon element
   const favicon = document.querySelector('link[rel="shortcut icon"');
@@ -15,4 +16,25 @@ const setFavicon = url => {
   }
 };
 
-setFavicon('./icon-2.png');
+// setFavicon('./icon-2.png');
+
+// use an emoji as the favicon
+const emojiFavicon = function (emoji) {
+  // create a canvas element
+  const canvas = document.createElement('canvas');
+  canvas.height = 64;
+  canvas.width = 64;
+
+  // get the canvas context
+  const context = canvas.getContext('2d');
+  context.font = '64px serif';
+  context.fillText(emoji, 0, 64);
+
+  // get the custom URL
+  const url = canvas.toDataURL();
+
+  // update the favicon
+  setFavicon(url);
+};
+
+emojiFavicon(`🎉🎉🎉`)
